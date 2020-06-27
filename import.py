c6a1418db7c1f4e94d27b1201048a9736d8f2727
Ankitha Anil <56465597+ankitha-anil@users.noreply.github.com>
2020-06-27 13:28:14 +0400

    Update import.py

--- import.py
+++ import.py
@@ -2,7 +2,6 @@ import os
 
 from sqlalchemy import create_engine
 from sqlalchemy.orm import scoped_session, sessionmaker
-from sqlalchemy import Table, Column, Float, String, Integer, MetaData
 
 engine = create_engine(os.getenv("DATABASE_URL")) # database engine object from SQLAlchemy that manages connections to the database
 db = scoped_session(sessionmaker(bind=engine))    # create a 'scoped session' that ensures different users' interactions with the
